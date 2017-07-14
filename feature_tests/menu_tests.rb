331d62ae00db94decbc29457cdf76f7fd4ddbde1
annalaise <annalaise.gibbons@gmail.com>
2017-07-14 19:15:37 +0100

    plan for first feature

---
+++ feature_tests/menu_tests.rb
@@ -0,0 +1,15 @@
+# USER STORY
+# As a customer
+# So that I can check if I want to order something
+# I would like to see a list of dishes with prices
+
+
+# PLAN
+# 1. Create CSV file of dishes and prices
+# 2. Create rspec test suite
+# 3. Create script for menu, ensuring items can be accessed from other classes using
+# some form of dish ID (?)
+# 4. Feature test in IRB throughout
+
+menu = Menu.new # research how to create Modules as this may be more suitable
+menu.print_menu
