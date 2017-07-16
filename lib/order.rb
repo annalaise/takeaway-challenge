9afcac4d13999f16a0ba6d6c394b585c696464b7
annalaise <annalaise.gibbons@gmail.com>
2017-07-16 20:23:36 +0100

    test for ordering unlimited items in Order

--- lib/order.rb
+++ lib/order.rb
@@ -1,6 +1,6 @@
 class Order
 
-def create
+def create(*items)
 end
 
 
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -7,4 +7,8 @@ it 'passes basic tests' do
   expect(order).to respond_to(:create)
 end
 
+it 'allows customer to order multiple items' do
+  expect(order).to respond_to(:create).with_unlimited_arguments
+end 
+
 end
